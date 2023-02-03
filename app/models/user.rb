c6629cc23878a242ffeaac39667aff3b747ecf65
Fanny <venezia.fanny@gmail.com>
2023-02-03 14:57:19 +0100

    Model User modifié

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :cars, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :reviews
+  has_many :messages
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
